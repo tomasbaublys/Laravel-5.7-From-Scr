60e662038365f8abc61079bd9d86a1b1f07c4061
vagrant <vagrant@homestead>
2019-03-14 09:08:26 +0000

    Initial Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mail Page</title>
+</head>
+<body>
+    <h1>Main Page</h1>
+    <p>
+        <a href="/contact">Contact</a> us to learn more
+    </p>
+</body>
+</html>
