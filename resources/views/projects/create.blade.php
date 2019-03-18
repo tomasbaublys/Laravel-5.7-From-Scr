0b2b3d78485b2364215962d530f91e07b175257c
vagrant <tomas@forumcinemashe.lt>
2019-03-18 16:12:41 +0000

    Add additional class to CREATE Project form for Bumla CSS framework

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -4,21 +4,31 @@
 
 @section('content')
 
-<h1>Create a New Project</h1>
+<h1 class="title">Create a New Project</h1>
 
 <form action="/projects" method="POST">
 	{{ csrf_field() }}
-	<div>
-		<input type="text" name="title" placeholder="Project title">
+	<div class="field">
+		<label class="label" for="title">Title</label>
+
+		<div class="control">
+			<input type="text" class="input" name="title">
+		</div>
 	</div>
 
-	<div>
-		<textarea name="description" placeholder="Project description"></textarea>
-	</div>
-	
-	<div>
-		<button type="submit">Create Project</button>
+	<div class="field">
+		<label class="label" for="description">Description</label>
+
+		<div class="control">
+			<textarea name="description" class="textarea"></textarea>
+		</div>
 	</div>	
+	
+	<div class="field">
+		<div class="control">
+			<button type="submit" class="button is-link">Create Project</button>
+		</div>
+	</div>
 </form>
 
 @endsection
